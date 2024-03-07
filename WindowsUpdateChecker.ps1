90549ac28958264a4ca9b365d398d5c1140b2ec4
Loffler-NOC <150292433+Loffler-NOC@users.noreply.github.com>
2024-03-07 09:12:09 -0600

    Adding manual update parsing
    Changing how updates are parsed and output in order to see "reboot required" flag.

--- WindowsUpdateChecker.ps1
+++ WindowsUpdateChecker.ps1
@@ -4,7 +4,8 @@ $providerInstalled = Get-PackageProvider -Name $providerName -ErrorAction Silent
 
 if ($providerInstalled) {
     Write-Host "NuGet package provider is installed."
-} else {
+}
+else {
     Write-Host "NuGet package provider is not installed. Attemping to install it now."
     Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 }
@@ -15,14 +16,90 @@ $installedModules = Get-Module -ListAvailable | Where-Object { $_.Name -eq $modu
 
 if ($installedModules) {
     Write-Host "The '$moduleName' module is installed."
-} else {
+}
+else {
     Write-Host "The '$moduleName' module is not installed. Attemping to install it now."
     Install-Module -Name PSWindowsUpdate -Force
 }
 
 Write-Host "Importing PSWindowsUpdate module"
 Import-Module PSWindowsUpdate
+
+
 Write-Host "Checking what updates are available"
-Get-WindowsUpdate
+Write-Output "-----------------------------"
+Write-Output "Windows update legend"
+Write-Output @"
+isDownloaded = D
+isInstalled = I
+isMandatory = M
+isHidden = H
+isUninstallable = U
+isBeta = B
+"@
+Write-Output "-----------------------------"
+
+# Save Get-WindowsUpdate to a variable
+$updates = Get-WindowsUpdate
+
+if ($updates.Count -ne 0) {
+
+    # Loop through each update and output desired fields
+    foreach ($update in $updates) {
+        $kb = $update.KB
+        $status = $update.Status
+        $title = $update.Title
+        $description = $update.Description
+        $rebootRequired = $update.RebootRequired
+
+        Write-Output "KB: $kb"
+        Write-Output "Status: $status"
+        Write-Output "Title: $title"
+        Write-Output "Description: $description"
+        Write-Output "Reboot Required: $rebootRequired"
+        Write-Output "-----------------------------"
+    }
+}
+else {
+    Write-Output "There are currently no updates available"
+    Write-Output "-----------------------------"
+}
+
+
 Write-Host "Checking what updates are installed"
-Get-WindowsUpdate -IsInstalled
+Write-Output "-----------------------------"
+Write-Output "Windows update legend"
+Write-Output @"
+isDownloaded = D
+isInstalled = I
+isMandatory = M
+isHidden = H
+isUninstallable = U
+isBeta = B
+"@
+Write-Output "-----------------------------"
+
+# Save Get-WindowsUpdate to a variable
+$updates = Get-WindowsUpdate -IsInstalled
+
+if ($updates.Count -ne 0) {
+    
+    # Loop through each update and output desired fields
+    foreach ($update in $updates) {
+        $kb = $update.KB
+        $status = $update.Status
+        $title = $update.Title
+        $description = $update.Description
+        $rebootRequired = $update.RebootRequired
+
+        Write-Output "KB: $kb"
+        Write-Output "Status: $status"
+        Write-Output "Title: $title"
+        Write-Output "Description: $description"
+        Write-Output "Reboot Required: $rebootRequired"
+        Write-Output "-----------------------------"
+    }
+}
+else {
+    Write-Output "There are currently no updates installed"
+}
